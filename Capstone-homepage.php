d3ed548e0e73fa1988b5ab1d9d865ce9a183d8bc
CJF86 <77642189+CJF86@users.noreply.github.com>
2023-03-29 20:32:56 -0400

    Add files via upload

---
+++ Capstone-homepage.php
@@ -0,0 +1,115 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "Tt5b453h@66";
+$database = "post-db";
+$UserDB = "user-info";
+$NotloggedIn = "no";
+
+session_start();
+
+echo '<link rel="stylesheet" href="HomepageCSS.css" />';
+
+$user_id = $_SESSION['user_ID'];
+$conn = new mysqli($servername,$username,$password,$database);
+$Userconn = new mysqli($servername,$username,$password,$UserDB);
+
+$NoLogin = "UPDATE users SET loginStatus = '$NotloggedIn' WHERE Username = '$user_id'";
+
+if($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+
+}
+
+$postRefresh = "SELECT * FROM posts";
+
+$postRefreshquery = mysqli_query($conn,$postRefresh);
+
+if(isset($_POST['submit']))
+{
+    $postData = mysqli_real_escape_string($conn,$_POST['info']);
+
+    $postCommand = "INSERT INTO posts (Post, postedby) VALUES ('$postData','$user_id')";
+
+    $postQuery = mysqli_query($conn,$postCommand);
+
+    header('location:Capstone-homepage.php');
+    
+}
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="HomepageCSS.css" />
+    <title></title>
+</head>
+<body>
+    
+
+    <div class="TopNav">
+        
+        <form action="" method="post" enctype="multipart/form-data">
+            <input type="submit" name="Logout" value="log in"/>
+            <input type="submit" name="Logout" value="Register"/>
+            <input type="submit" name="Logout" value="log out"/>
+        </form>
+        <?php  
+        if(isset($_POST['Logout']))
+        {
+            $LoginUpdateQuery = mysqli_query($Userconn,$NoLogin);
+            header('location:Capstone-login.php');
+
+        }
+
+        ?>
+        <?php echo '<a href="Capstone-userPage.php" class="User">Welcome back '.$user_id.'!</a>' ?>
+    </div>
+
+    <div class="Posts">
+        <h1>Latest posts</h1>
+
+        <div class="latestPosts">
+            
+            <?php
+            
+            $postIteration = mysqli_fetch_assoc($postRefreshquery);
+            
+            
+
+            while($postIteration = mysqli_fetch_assoc($postRefreshquery))
+            {
+                echo '<div class="postFormat">';
+                
+                    echo '<p>'.$postIteration["postedby"].'<br>'.$postIteration["Post"].'</p>';
+                
+                echo '</div>';
+
+            }
+
+            ?>
+            
+        </div>
+        
+
+        <form action="" method="post" enctype="multipart/form-data">
+
+            <input type="text" name="info" placeholder="What's on your mind?" class="box"/>
+
+            <input type="submit" name="submit" value="Post" class="btn"/>
+
+        </form>
+
+    </div>
+
+    
+    
+    
+
+</body>
+</html>
---
+++ Capstone-login.php
@@ -0,0 +1,103 @@
+﻿<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "Tt5b453h@66";
+$database = "user-info";
+$IsloggedIn = "yes";
+$NotloggedIn = "no";
+$conn = new mysqli($servername,$username,$password,$database);
+
+session_start();
+
+if($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+
+}
+
+if(isset($_POST['submit']))
+{
+    $name = mysqli_real_escape_string($conn,$_POST['name']);
+    $password = mysqli_real_escape_string($conn,$_POST['password']);
+    $LoginCommand = "SELECT * FROM users WHERE Username = '$name' AND Password = '$password'";
+    $LoginUpdate = "UPDATE users SET loginStatus = '$IsloggedIn' WHERE Username = '$name' AND Password = '$password'";
+    $NoLogin = "UPDATE users SET loginStatus = '$NotloggedIn' WHERE Username = '$name' AND Password = '$password'";
+    $LoginQuery = mysqli_query($conn,$LoginCommand);
+    $LoginUpdateQuery = mysqli_query($conn,$LoginUpdate);
+   
+
+    if(mysqli_num_rows($LoginQuery)==0)
+    {
+        $error[] = 'Please create an account';
+
+    }
+    else if(mysqli_num_rows($LoginQuery)>0)
+    {
+        $CurrentUser = mysqli_fetch_assoc($LoginQuery);
+        if($CurrentUser['loginStatus']==$IsloggedIn)
+        {
+            $error[] = 'This user has already logged in';
+        }
+        else
+        {
+            $_SESSION['user_ID'] = $CurrentUser['Username'];
+            header('location:Capstone-homepage.php');
+        }
+    }
+
+    
+
+}
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="LoginCSS.css" />
+    <title></title>
+</head>
+<body>
+    
+
+    <div class="header">
+        
+    </div>
+
+    <div class="Login">
+        
+        
+        <form action="" method="post" enctype="multipart/form-data">
+            <h1>Welcome!</h1>
+            <?php
+            if(isset($error))
+            {
+                foreach($error as $error)
+                {
+                    echo '<span class="error-msg">'.$error.'</span>';
+
+                }
+            }
+
+            ?>
+            
+            <input type="text" name="name" placeholder="username" class="box"/>
+            <input type="text" name="password" placeholder="password" class="box"/>
+            <p>Don't have an account? register <a href="Capstone-registration.php">here</a></p>
+            <input type="submit" name="submit" value="Login" class="btn"/>
+            
+
+        </form>
+
+
+        
+
+    </div>
+    
+    
+
+</body>
+</html>
